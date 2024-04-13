0895fae8c4a645cb63bd1583bd05467ecc3f46bd
filippogiovannoni <filippogiovannonii@gmail.com>
2024-04-13 11:21:04 +0200

    add methods for Post.php and Media.php

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -2,10 +2,16 @@
 
 class Media
 {
-    function __construct(public int $id, public string $type, public string $path)
+    function __construct(public int $id, public string $type, public string $path, public int $size)
     {
         $this->id = $id;
         $this->type = $type;
         $this->path = $path;
+        $this->size = $size;
+    }
+
+    public function getSize()
+    {
+        return $this->size;
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,8 @@
 class Post
 {
 
+    public $comments = [];
+
     function __construct(public int $id, public string $title, public Media $media, public string $date, public string $tags, public string $created_at, public string $updated_at)
     {
         $this->id = $id;
@@ -11,5 +13,10 @@ class Post
         $this->tags = $tags;
         $this->created_at = $created_at;
         $this->updated_at = $updated_at;
+    }
+
+    public function addComment($comment)
+    {
+        $this->comments[] = $comment;
     }
 };
